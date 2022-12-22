653160d13f1e7b353e2dd4b31792e5b7987d6866
Maycon <maycontss@gmail.com>
2022-12-22 17:08:36 -0300

    Criação do repositório

---
+++ src/OlaMundo.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+
+public class OlaMundo {
+    public static void main (String[] args){
+        int j = 10;
+        JOptionPane.showMessageDialog(null, "olá Mundo ");
+
+
+
+    }
+
+
+
+
+}
