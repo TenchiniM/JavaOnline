ee0f27e23640cfa3ba2b45dae9fa280f035f0622
Maycon <maycontss@gmail.com>
2022-12-22 19:30:09 -0300

    Acrescentando váriavel nome

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 public class OlaMundo {
     public static void main (String[] args){
         int j = 10;
-        JOptionPane.showMessageDialog(null, "olá Mundo ");
+        String nome = "Marcelo";
+        JOptionPane.showMessageDialog(null, "olá Mundo" + " " + nome);
+
         
 
 
