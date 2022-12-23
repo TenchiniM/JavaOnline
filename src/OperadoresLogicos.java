17e516e23d96d7215c8ddd0a75cbbb529312e519
Maycon <maycontss@gmail.com>
2022-12-23 12:07:37 -0300

    Adicionando OperadoresLogicos

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int idade = 18;
+        float salario = 1000f;
+        System.out.println( salario != 1000f);
+
+    }
+
+}
