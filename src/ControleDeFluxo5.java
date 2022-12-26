f18e046696ac3e34ed9e54d949bdde35d4084ac5
Maycon <maycontss@gmail.com>
2022-12-26 14:37:33 -0300

    Inserindo ControleDeFluxo4 e ControleDeFluxo5, Sobre os comandos BREAK e CONTINUE

---
+++ src/ControleDeFluxo5.java
@@ -0,0 +1,13 @@
+public class ControleDeFluxo5 {
+    public static void main(String[] args) {
+        // comando continue
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela >= 1000) {
+                continue; //ignora qualquer coisa que estiver abaixo no IF para preservar memoria
+            }
+            System.out.println("parcelas: " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
