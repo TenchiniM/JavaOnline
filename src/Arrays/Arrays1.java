e653cce5f59f263f1363f3c73d44e072dbd8014f
Maycon <maycontss@gmail.com>
2022-12-26 17:31:52 -0300

    Criação de Arrays2 e mais conteudos de arrays

---
+++ src/Arrays/Arrays1.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import javax.swing.*;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        //sintaxe para se criar um array
+        //arrays sao considerados objetos
+        //"new" se usa para objetos
+        int[] idades = new int[3];
+        idades[0] = 20;
+        idades[1] = 30;
+        idades[2] = 23;
+        JOptionPane.showMessageDialog(null, idades[0]);
+    }
+}
---
+++ src/Arrays/Arrays2.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import javax.swing.*;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+        // o valor padrão do array para int,byte, short , long, float e double é : 0
+        // para char é um valor vazio
+        // para boolean é false
+        // para reference é null
+        // nome do array.lenght - retorna o tamanho do array
+        String[] nomes = new String[5];
+        nomes[0] = "Luffy";
+        nomes[1] = "Maycon";
+        nomes[2] = "Fred";
+        nomes[3] = "Zoro";
+        nomes[4] = "Usoup";
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+            continue;
+        }
+        JOptionPane.showMessageDialog(null, nomes[0] + "\n" + nomes[1] + "\n" + nomes[2] + "\n" + nomes[3] + "\n" + nomes[4]);
+        nomes = new String[10]; // referenciando novamente o array fazendo ele ter 10 valores
+
+    }
+}
