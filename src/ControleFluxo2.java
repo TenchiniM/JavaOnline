70dae4a654424a832ed7357c2d2099f15917d5ea
Maycon <maycontss@gmail.com>
2022-12-26 11:09:00 -0300

    Realizando exercicio numero 3 no controle de fluxo 2 (adicionar se é dia util ou nao])

--- src/ControleFluxo2.java
+++ src/ControleFluxo2.java
@@ -8,7 +8,7 @@ public class ControleFluxo2 {
 
         switch (dia) {
             case 1:
-                JOptionPane.showMessageDialog(null, "Domingo");
+                JOptionPane.showMessageDialog(null, "Domingo (NÃO é dia útil)");
                 break;
             case 2:
                 JOptionPane.showMessageDialog(null, "Segunda");
@@ -26,17 +26,17 @@ public class ControleFluxo2 {
                 JOptionPane.showMessageDialog(null, "Sexta");
                 break;
             case 7:
-                JOptionPane.showMessageDialog(null, "Sabado");
+                JOptionPane.showMessageDialog(null, "Sabado (NÃO é dia útil)");
                 break;
         }
-        switch (sexo) {
-            case 'F':
-                JOptionPane.showMessageDialog(null, "Feminino");
-                break;
-            case 'M':
-                JOptionPane.showMessageDialog(null, "Masculino");
-            default:
-                JOptionPane.showMessageDialog(null, "Invalido");
-        }
+        // switch (sexo) {
+        //case 'F':
+        //JOptionPane.showMessageDialog(null, "Feminino");
+        //  break;
+        //case 'M':
+        //JOptionPane.showMessageDialog(null, "Masculino");
+        //default:
+        //  JOptionPane.showMessageDialog(null, "Invalido");
     }
+}
 }
