0a2fc56919ec0d094e80461ee334a5332a7ed188
Maycon <maycontss@gmail.com>
2022-12-26 14:04:24 -0300

    Acrescentando a classe ExercicioControleDeFluxo3 e realizando exercicio de repetição

--- src/ControleDeFluxo3.java
+++ src/ControleDeFluxo3.java
@@ -2,12 +2,12 @@ public class ControleDeFluxo3 {
     public static void main(String[] args) {
         //DO WHILE estrutura de repetição
         int contador = 0;
-        while (contador <= 10){
-            contador ++ ;
+        while (contador <= 10) {
+            contador++;
             System.out.println("contador funcionando");
         }
         // estrutura FOR
-        for (int i = 0; i<=100; i++)
+        for (int i = 0; i <= 100; i++)
             System.out.println("oi");
     }
 }
---
+++ src/ExercicioControleDeFluxo3.java
@@ -0,0 +1,11 @@
+public class ExercicioControleDeFluxo3 {
+    public static void main(String[] args) {
+        //imprimir os números pares de 1 até 100,000
+        int numeros = 0;
+        int valor = 100000;
+        for (int i = 0; i<=valor; i= i+2){
+            System.out.println(i);
+        }
+
+    }
+}
