0325729385f99f21c7af1d94439d0643af857a4e
tenchiniM <maycontss@gmail.com>
2023-01-03 07:53:46 -0300

    Iniciando metodos com retorno

--- src/br/com/abc/javacore/introducaometodos/classes/Calculadora.java
+++ src/br/com/abc/javacore/introducaometodos/classes/Calculadora.java
@@ -9,8 +9,29 @@ public class Calculadora {
     public void subtraiDoisNumeros() {
         System.out.println(5 - 5);
     }
-    public void multiplicaDoisNumeros(int num1, int num2){ // metodo recebendo uma variavel local
+
+    public void multiplicaDoisNumeros(int num1, int num2) { // metodo recebendo uma variavel local
         System.out.println(num1 * num2);
+
+    }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        // double resultado = num1 / num2; // um jeito de retorno
+        // return resultado;
+        // System.out.println(num1/num2); metodos com retorno vc imprime na classe onde ele é chamado
+        //return num1 / num2; // outro jeito de retorno
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void imprimeDoisNumerosDivididos(double num1, double num2) {
+        if (num2 != 0) {
+            System.out.println(num1 / num2);
+            return;
+        }
+            System.out.println("Não foi possivel dividir esse valor");
 
     }
 }
--- src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
+++ src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
@@ -5,10 +5,17 @@ import br.com.abc.javacore.introducaometodos.classes.Calculadora;
 public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
+        System.out.println("Somando Dois Numeros");
         calc.somaDoisNumeros();
-        System.out.println("Continuando a execução");
+        System.out.println("Subtrai dois numeros");
         calc.subtraiDoisNumeros();
+        System.out.println("Multiplica dois numeros");
         calc.multiplicaDoisNumeros(10, 20);
+        System.out.println("Divide dois numeros");
+        System.out.println(calc.divideDoisNumeros(20, 0));
+        System.out.println("Imprime dois numeros divididos");
+        calc.imprimeDoisNumerosDivididos(20,5);
+        System.out.println("Continuando a execução");
     }
 
 }
